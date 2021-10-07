4348998f78c7c802b1b849e56557d6e13c99058a
regireis7 <regi@fisastrategy.com>
2021-10-07 22:14:31 +0100

    @caching -> @EnableCaching

--- src/main/java/com/regiBoot/config/RedisConfig.java
+++ src/main/java/com/regiBoot/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.regiBoot.config;
 
 import org.springframework.cache.annotation.Caching;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -10,7 +11,7 @@ import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@Caching
+@EnableCaching
 public class RedisConfig {
 
     @Bean
